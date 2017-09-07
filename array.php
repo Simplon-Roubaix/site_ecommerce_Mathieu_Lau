9fceb6556092c401c213714bb10fcbf9ac593ccd
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-09-07 14:02:44 +0200

    add array.php

---
+++ array.php
@@ -0,0 +1,22 @@
+<?php
+$text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+$text_long="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a fringilla dui. Mauris suscipit purus at sem consequat volutpat. Nullam placerat finibus mauris, a laoreet elit viverra vitae. Maecenas eget felis velit. Praesent faucibus purus et blandit porta. Aliquam vestibulum, elit in ultricies varius, ligula augue luctus lectus, vel malesuada mi leo blandit urna. Proin aliquam eros non dapibus commodo. Mauris euismod viverra quam in malesuada. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer aliquam ultrices accumsan. Phasellus egestas sapien eget dictum porta. Fusce volutpat scelerisque lectus ut iaculis.
+
+Cras sit amet purus feugiat, dignissim turpis in, pharetra mauris. Praesent vel imperdiet ante. Curabitur non enim et sapien efficitur laoreet. Proin nec ipsum et purus cursus blandit. Nunc viverra ornare faucibus. Donec ut mi sem. Cras porta pellentesque massa.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam erat volutpat. Nulla in nulla nisi. Etiam pulvinar efficitur erat nec fermentum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur lacinia sodales elit vitae ultricies. Aenean at turpis vel leo luctus pharetra.
+
+Maecenas pharetra sit amet enim fermentum gravida. Phasellus mollis ac elit sit amet efficitur. Aenean convallis at elit ultricies pulvinar. Phasellus purus lacus, venenatis et lorem non, semper semper neque. Nullam hendrerit neque at velit venenatis aliquet. Suspendisse dignissim odio mi, id mattis orci convallis at. Quisque varius massa enim, vel pharetra tellus rhoncus nec. Donec mattis enim sit amet pharetra efficitur.
+
+Sed ac magna ante. Etiam nulla dolor, bibendum a euismod efficitur, maximus sed libero. Aliquam rhoncus placerat pharetra. Nunc id erat ut ex semper vulputate eu et leo. Maecenas risus nibh, interdum sed ultricies at, tincidunt sed enim. Donec a elit tristique, viverra lacus vel, dignissim ligula. Ut placerat massa tortor, eget placerat mauris tincidunt at. In ut urna vitae risus vestibulum volutpat ac a augue. Maecenas a euismod mi. Duis lacinia non lorem in feugiat. Donec blandit non nulla convallis ultrices. Duis accumsan purus id diam hendrerit, vitae lobortis felis auctor. Fusce fermentum volutpat enim nec laoreet. Vestibulum fermentum tincidunt elit, a porta sem eleifend in. Vivamus sollicitudin interdum nibh. Pellentesque dui sapien, rutrum ut faucibus ac, blandit at libero.";
+
+$dogs = array (
+  array ("img"=>"img/boxer.png", "nom"=>"Jojo","description" =>$text , "file"=> $text_long),
+  array ("img"=>"img/dog.jpg", "nom"=>"Aaron","description" =>$text , "file"=> $text_long),
+  array ("img"=>"img/lab.jpg", "nom"=>"Napoleon","description" =>$text , "file"=> $text_long),
+  array ("img"=>"img/labrador.jpg", "nom"=>"Nanouk","description" =>$text , "file"=> $text_long),
+  array ("img"=>"img/puppy.jpg", "nom"=>"Raoul","description" =>$text , "file"=> $text_long),
+  array ("img"=>"img/shiba.jpg", "nom"=>"Okapi","description" =>$text , "file"=> $text_long),
+);
+ ?>
