2c834453b0a6883622de3b4954dd2bc2dc2874e4
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-09-15 16:43:23 +0200

    comment

--- formulaire_post.php
+++ formulaire_post.php
@@ -1,10 +1,10 @@
-<!-- <?php $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8', 'root', 'gj7b!17LA');
+<?php $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8', 'root', 'gj7b!17LA');
 // $bdd = new PDO('mysql:host=localhost;dbname=LAU&MATH;charset=utf8', 'root', 'root');
 
 
-  $req = $bdd->prepare('INSERT INTO articles SET id_img=?, nom=?, description=?, grosse_description=?');
-  $req->execute([$_POST['id_img'], $_POST['nom'],$_POST['description'], $_POST['grosse_description']]);
-?> -->
+  // $req = $bdd->prepare('INSERT INTO articles SET id_img=?, nom=?, description=?, grosse_description=?');
+  // $req->execute([$_POST['id_img'], $_POST['nom'],$_POST['description'], $_POST['grosse_description']]);
+?>
 
 <?php
     function transfert(){
