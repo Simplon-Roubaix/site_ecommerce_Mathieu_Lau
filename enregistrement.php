0773887a990bca4e58c43cf87fe9f530dbb85e9f
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-09-19 09:55:13 +0200

    add button article

---
+++ enregistrement.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class=""action="enregistrement_post.php" method="post">
+    <input type="text" name="pseudo" placeholder="pseudo" value="">
+    <input type="password" name="password" placeholder="password" value="">
+    <p><input type="submit" name=""value="S'inscrire"></p> </form>
+
+
+  </body>
+</html>
